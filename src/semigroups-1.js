13a2ac6c7fbbe2b9677cdc22ea2b54dc11e3c058
Thomas Greco <tgreco1610@gmail.com>
2020-05-17 01:38:43 -0400

    added code for semigroups 2

--- src/semigroups-1.js
+++ src/semigroups-1.js
@@ -6,22 +6,26 @@ const Sum = value => ({
   concat: y => Sum(value + y.value)
 });
 
-test('Sum(10)', async assert => {
-  const given = '.concat(Sum(20))';
+const Product = value => ({
+  value,
+  concat: y => Product(value * y.value)
+})
 
+test("Product(10) Semigroup", async assert => {
+  const result = Product(10).concat(Product(5)).concat(Product(2));
   assert({
-    given,
-    should: 'return an object with a value of 30.',
-    actual: Sum(10).concat(Sum(20)).value,
-    expected: 30
-  });
-
-  const first = Sum(10).concat(Sum(20).concat(Sum(15)));
-  const second = Sum(10).concat(Sum(20)).concat(Sum(15));
-  assert({
-    given: ".concat(Sum(20).concat(Sum(15)))",
-    should: 'be associative.',
-    actual: equals(first.value, second.value),
+    given: ".concat(Product(5)).concat(Product(2))",
+    should: "return a value of 100",
+    actual: equals(result.value, 100),
     expected: true
   });
-});
+
+  const first = Product(10).concat(Product(5)).concat(Product(2));
+  const second = Product(10).concat(Product(5).concat(Product(2)));
+  assert({
+    given: ".concat(Product(5).concat(Product(2))",
+    should: "return a value of 100 regardless of where the concattenation takes place",
+    actual: equals(first.value, second.value),
+    expected: true
+  })
+})
