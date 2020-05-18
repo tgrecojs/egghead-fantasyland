0fadc0b97c96d32c904ea6e15a8a6b86c5ac6b00
Thomas Greco <tgreco1610@gmail.com>
2020-05-18 11:52:21 -0400

    added code from monoid example

--- src/monoid.js
+++ src/monoid.js
@@ -1,65 +1,51 @@
 import test from "riteway";
 
-const trace = label => value => {
-  console.log(`${label}::`, value);
-  return value
-};
-
 const Product = value => ({
   value,
   concat: y => Product(value * y.value)
 });
+
 Product.empty = () => Product(1);
 
-const Max = x => ({
-  x,
-  concat: y => Max(Math.max(x, y.x))
+const Sum = value => ({
+  value,
+  concat: y => Sum(value + y.value)
 });
-
-Max.empty = () => Max(-Infinity);
-
-const Sum = x => ({
-  x,
-  concat: y => Sum(x + y.x)
-});
-
 
 Sum.empty = () => Sum(0);
 
-const result = Product.empty().concat(Product(10))
-
-// A friendly neighbourhood monoid fold.
-// fold :: Monoid m => (a -> m) -> [a] -> m
-const fold = M => data => data.reduce(
-  (acc, x) => {
-    console.log({ x, acc })
-    return acc.concat(M(x))
-  },
-  M.empty())
-
-// We can now use our monoids for (almost) all
-// our array reduction needs!
-const res = fold(Sum)([]) // 15
-
-test('', async assert => {
+test('Product Monoid', async assert => {
+  const inputValue = 20;
+  const result = Product(inputValue);
   assert({
-    given: '',
-    should: '',
-    actual: '',
-    expected: ''
-  })
-})
-test("Product(10).concat", async assert => {
+    given: 'right identity',
+    should: 'return a monoid with its value',
+    actual: result.concat(Product.empty()).value === inputValue,
+    expected: true
+  });
+
   assert({
-    given: "Product(5) & Product(2)",
-    should: "return the same value regardless of how .concat is called",
-    actual: result.value,
-    expected: 3
-  })
+    given: 'left identity',
+    should: 'return a monoid with its value',
+    actual: Product.empty().concat(result).value === inputValue,
+    expected: true
+  });
+});
+
+test('Sum Monoid', async assert => {
+  const inputValue = 20;
+  const result = Sum(inputValue);
   assert({
-    given: '',
-    should: '',
-    actual: '',
-    expected: ''
+    given: 'right identity',
+    should: 'return a monoid with its value',
+    actual: result.concat(Sum.empty()).value === inputValue,
+    expected: true
+  });
+
+  assert({
+    given: 'left identity',
+    should: 'return a monoid with its value',
+    actual: Sum.empty().concat(result).value === inputValue,
+    expected: true
   });
 });
