171a39cd639fb9a98ee99822b971e83cd9140e6f
Thomas Greco <tgreco1610@gmail.com>
2020-05-18 11:42:11 -0400

    added code for 1st semigroups lesson

--- src/semigroups-1.js
+++ src/semigroups-1.js
@@ -1,31 +1,8 @@
-import test from "riteway";
-import { equals } from "ramda";
-
 const Sum = value => ({
   value,
-  concat: y => Sum(value + y.value)
+  concat: y => Sum(valuegu + y.value)
 });
 
-const Product = value => ({
-  value,
-  concat: y => Product(value * y.value)
-})
-
-test("Product(10) Semigroup", async assert => {
-  const result = Product(10).concat(Product(5)).concat(Product(2));
-  assert({
-    given: ".concat(Product(5)).concat(Product(2))",
-    should: "return a value of 100",
-    actual: equals(result.value, 100),
-    expected: true
-  });
-
-  const first = Product(10).concat(Product(5)).concat(Product(2));
-  const second = Product(10).concat(Product(5).concat(Product(2)));
-  assert({
-    given: ".concat(Product(5).concat(Product(2))",
-    should: "return a value of 100 regardless of where the concattenation takes place",
-    actual: equals(first.value, second.value),
-    expected: true
-  })
-})
+console.log(
+  Sum(20).concat(Sum(10))
+)
